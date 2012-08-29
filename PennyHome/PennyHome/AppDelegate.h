99363b62bc014a6b1708015b675867eca11386cd
PennyZheng <zhengzeping@foxmail.com>
2012-08-29 15:40:05 +0800

    newProject
    create new project

---
+++ PennyHome/PennyHome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PennyHome
+//
+//  Created by xu on 8/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PennyHome/PennyHome/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PennyHome
+//
+//  Created by xu on 8/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
